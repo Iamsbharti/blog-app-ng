ea9f2593b3679503c4b97fa1d454cb76aa47939c
om <om@omjha.local>
2020-05-29 03:05:00 +0530

    added blog-view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogViewComponent
   ],
   imports: [
     BrowserModule,

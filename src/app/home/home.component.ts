d9e88eff5af49f87966f9284e7e29a716d919f54
Saurabh <saurabhbharti9@gmail.com>
2020-05-30 17:37:37 +0530

    added home compoenent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  public allBlogs: any = [
+    {
+      id: 1,
+      name: 'blog1',
+      title: 'Focus',
+      sampleTest:
+        'The art of focus is simple, one with closed eye can see diffrent',
+      body:
+        'The art of focus is simple, one with closed eye can see diffrent perspective of oneself.It shows',
+      created: '30th May 2020',
+      author: 'saurabh',
+      bg: 'assets/brick-blue.jpg',
+    },
+    {
+      id: 2,
+      name: 'blog2',
+      title: 'Not there?',
+      sampleTest:
+        'The art of focus is simple, one with closed eye can see diffrent',
+      body:
+        'The art of focus is simple, one with closed eye can see diffrent perspective of oneself.It shows',
+      created: '29th May 2020',
+      author: 'John',
+      bg: 'assets/bg.jpg',
+    },
+    {
+      id: 3,
+      name: 'blog3',
+      title: 'Way to galaxy',
+      sampleTest:
+        'The art of focus is simple, one with closed eye can see diffrent',
+      body:
+        'The art of focus is simple, one with closed eye can see diffrent perspective of oneself.It shows',
+      created: '12th May 2020',
+      author: 'Jack',
+      bg: 'assets/woods.jpg',
+    },
+  ];
+  ngOnInit(): any {
+    console.log(this.allBlogs);
   }
-
 }
